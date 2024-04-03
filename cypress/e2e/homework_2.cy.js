42b17299163f55cbecb842c73b61c1dbb56f427c
KamilaCzarnecka <143633220+KamilaCzarnecka@users.noreply.github.com>
2024-04-03 11:31:01 +0200

    Add files via upload

---
+++ cypress/e2e/homework_2.cy.js
@@ -0,0 +1,16 @@
+describe("Praca domowa Modul 2", () => {
+  it("Wpisz email user888 i haslo 1234567890", () => {
+    cy.visit("https://www.edu.goit.global/account/login");
+    cy.login("user888@gmail.com", "1234567890");
+    cy.get('#open-navigation-menu-mobile').click();
+    cy.get(':nth-child(12) > .next-bve2vl').scrollIntoView().should("be.visible");
+    cy.get(':nth-child(12) > .next-bve2vl').click();
+  });
+
+  it("Wpisz email testowyqa i haslo QA!automation-1", () => {
+    cy.visit("https://www.edu.goit.global/account/login");
+    cy.login("testowyqa@qa.team", "QA!automation-1");
+    cy.get('#open-navigation-menu-mobile > svg > path').click();
+    cy.get(':nth-child(9) > .next-bve2vl').click();
+  });
+ });
